13054ea91a7a82564213f8cbbe878c081308b767
parziyval <el.luteci@gmail.com>
2022-01-14 21:40:47 +0100

    odstranenie zbytocnosti

--- App/Views/Auth/registraciaForm.view.php
+++ App/Views/Auth/registraciaForm.view.php
@@ -17,31 +17,28 @@
                 <form method="post" action="?c=auth&a=registruj">
                     <label for="regInputEmail" class="form-label text-dark">Email</label>
                     <div class="col-sm-10 pb-3">
-                        <input type="text" class="form-control" name="email" id="regInputEmail">
-                        <small>Chyba</small>
-                        <i class="bi bi-exclamation-circle-fill text-danger"></i>
-                        <i class="bi bi-check-circle-fill text-success"></i>
+                        <input type="text" class="form-control" name="email" id="regInputEmail" maxlength="70" required>
                     </div>
 
                     <label for="regInputMeno" class="form-label text-dark">Meno</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" name="meno" id="regInputMeno">
+                        <input type="text" class="form-control" name="meno" id="regInputMeno" maxlength="50" required>
                     </div>
 
                     <label for="regInputPriezvisko" class="form-label text-dark">Priezvisko</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" name="priezvisko" id="regInputPriezvisko">
+                        <input type="text" class="form-control" name="priezvisko" id="regInputPriezvisko" maxlength="50" required>
                     </div>
 
                     <label for="inputPassword" class="form-label text-dark">Heslo<br>
                         <small class="text-secondary">(Musí mať 8-30 znakov, musí obsahovať aspoň jedno veľké písmeno, aspoň jedno malé písmeno a aspoň jednu číslicu)</small></label>
                     <div class="col-sm-10">
-                        <input type="password" class="form-control" name="heslo" id="regInputPassword">
+                        <input type="password" class="form-control" name="heslo" id="regInputPassword" maxlength="30" required>
                     </div>
 
                     <label for="inputPassword2" class="form-label text-dark">Znova zadajte heslo</label>
                     <div class="col-sm-10 mb-3">
-                        <input type="password" class="form-control" name="heslo2" id="regInputPassword2">
+                        <input type="password" class="form-control" name="heslo2" id="regInputPassword2" maxlength="30" required>
                     </div>
 
                     <div class="form-group">
